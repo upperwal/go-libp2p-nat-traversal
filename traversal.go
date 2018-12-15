c40e298235161fcf7b62aad556308d19f9fec444
Abhishek Upperwal <a.upperwal@gmail.com>
2018-12-15 15:51:39 +0530

    print error

--- traversal.go
+++ traversal.go
@@ -246,6 +246,8 @@ func (b *NatTraversal) handleHolePunchRequest(m PacketWPeer) {
 		}
 		(*b.host).Network().(*swarm.Swarm).Backoff().Clear(pi.ID)
 
+		log.Info(err.Error())
+
 		if err.Error() == "no route to host" {
 			log.Info("Delay because of", err)
 			time.Sleep(time.Millisecond * 1500)
