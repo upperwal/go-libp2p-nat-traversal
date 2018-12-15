f6d73ceb66b352432483a95ecdf0cb041532c0dd
Abhishek Upperwal <a.upperwal@gmail.com>
2018-12-15 11:25:32 +0530

    client example

---
+++ example/client.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	logging "github.com/ipfs/go-log"
+	libp2p "github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	peer "github.com/libp2p/go-libp2p-peer"
+	ma "github.com/multiformats/go-multiaddr"
+
+	bootstrap "github.com/upperwal/go-libp2p-bootstrap"
+)
+
+func main() {
+	logging.SetLogLevel("bootstrap", "DEBUG")
+
+	port := flag.Int("p", 3000, "port number")
+	rp := flag.String("r", "", "remote peer id")
+	flag.Parse()
+
+	ctx := context.Background()
+
+	// libp2p.New constructs a new libp2p Host.
+	// Other options can be added here.
+	sourceMultiAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", *port))
+
+	host, err := libp2p.New(ctx, libp2p.ListenAddrs(sourceMultiAddr))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("This node: ", host.ID().Pretty(), " ", host.Addrs())
+
+	_, err = dht.New(ctx, host)
+	if err != nil {
+		panic(err)
+	}
+
+	b, _ := bootstrap.NewBootstrap(ctx, &host)
+
+	/* ma, _ := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/3000/p2p/QmSHQpWVzoGWiYRyBrikFp6tr8MAwm6RnUxPsu1NC2y8iJ")
+	pi, _ := pstore.InfoFromP2pAddr(ma) */
+	b.ConnectToServiceNodes(ctx, []string{"/ip4/127.0.0.1/tcp/3001/p2p/QmXDvZ7YsnjxuFZgCjEEwqVms5SiE2opG9vXzkQSid96yt"})
+
+	/* ma, _ := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/3000/p2p/QmVvYUj13isfoP4p9ppDZgboX9QwUDKkefP2nTGxVwfYBz")
+	pi, _ := pstore.InfoFromP2pAddr(ma) */
+	if *rp != "" {
+
+		p, err := peer.IDB58Decode(string(*rp))
+		fmt.Println(err)
+		fmt.Println("Conn to: ", p)
+
+		b.Connect(ctx, p)
+	}
+
+	select {}
+}
